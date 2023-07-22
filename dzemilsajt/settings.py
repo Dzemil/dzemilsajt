722d123f9d3ced7782eec3cefa9a2271fa9be5bd
Dzemil <dzemilpapic5@gmail.com>
2023-07-22 21:02:10 +0200

    added css

--- dzemilsajt/settings.py
+++ dzemilsajt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dvq&yf#4t1_zbu&augo(ylat7k@btt8+2+u(cy&g6pnigl%rav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = "static/"
