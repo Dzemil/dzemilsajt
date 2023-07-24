50d523993066c106294f8e6250b793b30caf0d4d
Dzemil <dzemilpapic5@gmail.com>
2023-07-24 12:59:44 +0200

    add post skeleton added

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,11 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
 
-class PostDetail(generic.DeleteView):
+class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+class PostNew(generic.CreateView):
+    model = Post
+    template_name = 'blog/add_post.html'
+    fields = '__all__'
