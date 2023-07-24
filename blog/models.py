6708cbb38dde4872dafa94c21995d8afadd507d9
Dzemil <dzemilpapic5@gmail.com>
2023-07-24 15:28:05 +0200

    added forms and add post functiunality

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 STATUS = (
     (0,"Draft"),
@@ -19,3 +20,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog')
